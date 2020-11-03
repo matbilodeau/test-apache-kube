bf99b3adbac9cc253a482c69832729ab04f58844
matbilodeau <matbilodeau@users.noreply.github.com>
2020-11-02 20:32:32 -0500

    Update entrypoint.sh
    fixed script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#echo '<!doctype html><html><head><meta charset="utf-8"><title>Où suis-je</title></head><body><h1>Voici un test Kubernetes</h1> <p>Vous êtes dans le pod ' |tee /var/www/html/index2.ht>
+#echo '<!doctype html><html><head><meta charset="utf-8"><title>Où suis-je</title></head><body><h1>Voici un test Kubernetes</h1> <p>Vous êtes dans le pod ' |tee /var/www/html/index2.html && echo $HOSTNAME | tee -a /var/www/html/index2.html && echo '</p></body></html>' | tee -a /var/www/html/index2.html
 
 /usr/sbin/apache2ctl -D FOREGROUND
